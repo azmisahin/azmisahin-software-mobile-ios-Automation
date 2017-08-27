06bfb2bcf9dbb13dc0942e3507c2011c808066aa
Azmi SAHIN <azmisahin@azmis-mac.local>
2017-08-27 16:41:20 -0700

    UILabelExtensions And Tests

---
+++ webTests/Extensions/UILabelExtensionsTests.swift
@@ -0,0 +1,87 @@
+//
+//  UILabelExtensionsTests.swift
+//  web
+//
+//  Created by Azmi SAHIN on 27/08/2017.
+//  Copyright © 2017 Azmi SAHIN. All rights reserved.
+//
+
+// MARK: - Requared
+import XCTest
+import UIKit
+
+@testable import web
+
+// MARK: - web Tests
+/// web Extentions Tests
+///
+/// `UILabel Extensions Tests`
+///
+class UILabelExtensionsTests: XCTestCase {
+    
+    // MARK: - Global Define
+    var expected: Bool!
+    var result: Bool!
+    var actual: Bool!
+    var mock : MockBase!
+    
+    
+    /// Test Setup
+    override func setUp() {
+        
+        // Base
+        super.setUp()
+        
+        // Define
+        expected = true
+        result = true
+        actual = true
+        mock = MockBase()
+    }
+    
+    /// Test Tear Down
+    override func tearDown() {
+        
+        // Base
+        super.tearDown()
+        
+        // Tear
+    }
+    
+    /// Test Font Size That Fits
+    func testFontSizeThatFits() {
+        
+        // Define
+        actual = false
+        
+        // Proc
+        actual = result
+        
+        // Test Process
+        let test = UILabel.init().fontSizeThatFits(text: "test")
+        print(test)
+        
+        // Assert
+        XCTAssertEqual(actual, expected,"Initalize Problem");
+    }
+    
+    // Test Application Performance
+    func testApplicationPerformance() {
+        
+        if( mock.testPerformance)
+        {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .full
+            dateFormatter.timeStyle = .full
+            
+            let date = NSDate()
+            
+            // Measure
+            self.measure {
+                
+                //
+                _ = dateFormatter.string(from: date as Date)
+            }
+        }
+    }
+}
