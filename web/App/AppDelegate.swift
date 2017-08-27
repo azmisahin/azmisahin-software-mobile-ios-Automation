2cd8f3cfb4ee0a7655267c04c14c7fe5fa8768f1
Azmi SAHIN <azmisahin@azmis-mac.local>
2017-08-27 10:55:00 -0700

    IOS Mobile Application Xcode View Design

---
+++ web/App/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  web
+//
+//  Created by Azmi SAHIN on 26/08/2017.
+//  Copyright © 2017 Azmi SAHIN. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+       
+    }
+    
+    
+}
+
